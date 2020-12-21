470d290d4730fb2f8fc327237be4c3618da7eab3
Jawad Jawid <jawadadham98@gmail.com>
2020-12-20 20:52:24 -0500

    Fix bug, and add ability to not modify DB

--- screenshot/index.js
+++ screenshot/index.js
@@ -3,36 +3,37 @@ const mongodb = require('mongodb');
 
 module.exports = async function (context, req) {
     //http://localhost:7071/api/screenshot?url=https://google.com&email=jawadadham98@gmail.com
+    //https://nodetracker.azurewebsites.net/api/screenshot?url=https://google.com&email=jawadadham98@gmail.com&modify=true
     const url = req.query.url || "https://google.com/";
     const email = req.query.email || "jawadadham98@gmail.com";
+    const modify = req.query.modify || false;
 
     const browser = await puppeteer.launch({
         args: ["--no-sandbox"]
     });
     const page = await browser.newPage();
     await page.goto(url);
-    const screenshotBuffer = await page.screenshot({ fullPage: true });
+    // const screenshotBuffer = await page.screenshot({ fullPage: true });
 
     const innerText = await page.evaluate(() => document.querySelector('body').innerText);
 
     await browser.close();
 
-    // const uri = "mongodb+srv://jawad:jawad@cluster0.r6ob1.azure.mongodb.net/test?retryWrites=true&w=majority";
-    // const client = new MongoClient(uri, { useNewUrlParser: true });
-
-    var MongoClient = require('mongodb').MongoClient;
-    var uri = "mongodb+srv://jawad:jawad@cluster0.r6ob1.azure.mongodb.net/test?retryWrites=true&w=majority";
-    
-    MongoClient.connect(uri, function(err, db) {
-      if (err) throw err;
-      var dbo = db.db("mydb");
-      var myobj = { url: url, email: email, text: innerText };
-      dbo.collection("customers").insertOne(myobj, function(err, res) {
+    if(modify){
+      var MongoClient = require('mongodb').MongoClient;
+      var uri = "mongodb+srv://jawad:jawad@cluster0.r6ob1.azure.mongodb.net/test?retryWrites=true&w=majority";
+      
+      MongoClient.connect(uri, function(err, db) {
         if (err) throw err;
-        console.log("1 document inserted to mongo");
-        db.close();
+        var dbo = db.db("mydb");
+        var myobj = { url: url, email: email, text: innerText };
+        dbo.collection("customers").insertOne(myobj, function(err, res) {
+          if (err) throw err;
+          console.log("1 document inserted to mongo");
+          db.close();
+        });
       });
-    });
+    }
     context.res = {
         body: innerText,
         headers: {
